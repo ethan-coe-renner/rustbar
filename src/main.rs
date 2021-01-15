1dd27a245b9472c1b5060da995683d12db92a4a5
E208114 <e.coerenner@gmail.com>
2021-01-14 20:08:10 -0800

    add color functionality for swaybar

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,83 @@
 use chrono::Local;
 
-
 use std::fs::File;
 use std::io::{BufReader, BufRead};
 use std::{thread, time};
 use std::process::Command;
 
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct Elem {
+    full_text: String,
+    color: String,
+}
+
+
+#[derive(Serialize)]
+struct Header {
+    version: u32,
+}
+
+#[derive(Serialize)]
+#[serde(untagged)]
+enum Block {
+    Line(Vec<Elem>),
+}
+
 fn main() {
     let delay = time::Duration::from_millis(6000);
+
+    let header = Header {
+	version: 1,
+    };
+    
+    println!("{}\n[\n[]\n", serde_json::to_string(&header).unwrap());
+    
     loop {
-	println!("UPD: {} | TODO: {} | NEWS: {} | VOL: {} | BAT: {} | {}",
-		 updates(),
-		 tasks(),
-		 news(),
-		 audio(),
-		 battery(),
-		 datetime(),
+
+	let line = vec!(
+	    updates(),
+	    tasks(),
+	    news(),
+	    audio(),
+	    battery(),
+	    datetime(),
 	);
+
+	let line = Block::Line(line);
+
+	println!(",{}", serde_json::to_string(&line).unwrap());
 
         thread::sleep(delay);
     }
 }
 
-fn datetime() -> String {
-    Local::now().format("%H:%M, %a %d %b %Y").to_string()
+fn datetime() -> Elem {
+    gen_elem("", &Local::now().format("%H:%M, %a %d %b %Y").to_string(), "#5e81ac")
 }
 
-fn battery() -> u32 {
-    let bat0path = "/sys/class/power_supply/BAT0/capacity";
-    let bat1path = "/sys/class/power_supply/BAT1/capacity";
+fn battery() -> Elem {
+    let bat0percent = read_num_from_file("/sys/class/power_supply/BAT0/capacity");
+    let bat1percent = read_num_from_file("/sys/class/power_supply/BAT1/capacity");
 
-    let bat0percent = read_num_from_file(bat0path);
-    let bat1percent = read_num_from_file(bat1path);
 
-    bat0percent + bat1percent
+    gen_elem("BAT: ", &(bat0percent + bat1percent).to_string(), "#d8dee9")
 }
 
-fn news() -> u32 {
-    read_num_from_file("~/.local/share/newsunread")
+fn news() -> Elem {
+    gen_elem("NEWS: ", &read_num_from_file("~/.local/share/newsunread").to_string(), "#d8dee9")
 }
 
-fn tasks() -> u32 {
-    read_num_from_file( "~/.local/share/tasks")
+fn tasks() -> Elem {
+    gen_elem("TODO: ", &read_num_from_file("~/.local/share/tasks").to_string(), "d8dee9")
 }
 
-fn updates() -> u32 {
-    read_num_from_file( "~/.local/share/updates")
+fn updates() -> Elem {
+    gen_elem("UPD: ", &read_num_from_file("~/.local/share/updates").to_string(), "d8dee9")
 }
 
-fn audio() -> String {
+fn audio() -> Elem {
     let volumeoutput = Command::new("pamixer")
         .arg("--get-volume")
 	.output()
@@ -64,9 +93,9 @@ fn audio() -> String {
     let muted: String = String::from_utf8(muteoutput.stdout).unwrap();
 
     match muted.as_ref() {
-	"false\n" => volume.to_string() + "%",
-	"true\n" => volume.to_string() + "% (muted)",
-	_ => "error".to_string()
+	"false\n" => gen_elem("VOL: ", &volume.to_string(), "#d8dee9"),
+	"true\n" => gen_elem("VOL: ", &volume.to_string(), "#4c566a"),
+	_ => gen_elem("VOL: ", &volume.to_string(), "#4c566a"),
     }
 }
 
@@ -82,4 +111,11 @@ fn read_num_from_file(filepath: &'static str) -> u32 {
     let _ = buffer.read_line(&mut line);
 
     line.chars().filter(|c| c.is_digit(10)).collect::<String>().parse::<u32>().unwrap()
+}
+
+fn gen_elem(name: &'static str, text: &str, color: &'static str) -> Elem {
+    Elem {
+	full_text: name.to_string() + text,
+	color: color.to_string()
+    }
 }
